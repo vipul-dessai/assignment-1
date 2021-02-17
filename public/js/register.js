9416822e56a34e0cfa290f83b5f2788cf1353cd3
tejasdha <tejas_dharmadhikari@persistent.com>
2021-02-17 12:24:24 +0530

    Registration Page with Routing Added

--- public/js/register.js
+++ public/js/register.js
@@ -1,1 +1,26 @@
-alert('h1');
+const express = require('express');
+
+
+//body parser
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/",function(req,res){
+    console.log(req.body);
+    res.send("Welcome User1 "+req.body.name);
+  
+    axios.post('https://jsonplaceholder.typicode.com/users', {
+        
+    })
+    .then(function (response) {
+        console.log(response)
+    })
+    .catch(function (error) {
+      
+    });
+    
+    
+
+});
+
--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,37 @@ const register = require('../routes/register');
 
 const indexController = require('../controllers/index');
 
+
+//body parser
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 router.use('/login', login);
 router.use('/cart', cart);
 router.use('/register', register);
 
 router.get('/', function (req, res, next) {
     indexController.renderDashboard(req, res, next);
+});
+
+
+router.post("/",function(req,res){
+    console.log(req.body);
+    res.send("Welcome User1 "+req.body.name);
+    axios.post('https://jsonplaceholder.typicode.com/users', {
+      
+    })
+    .then(function (response) {
+        console.log(response)
+    })
+    .catch(function (error) {
+      
+    });
+    
+    
+
 });
 
 module.exports = router;
